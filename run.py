98c55cd90953624a0a9c33b7575bafe7a8e6bcaa
Andrew Ke <keyanzhe@gmail.com>
2024-05-13 10:52:47 +0100

    import all 6 files

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ import csv
 import numpy as np
 
 # Open and read both CSV files
-with open('channel.csv', 'r') as channel_file, open('file1.csv', 'r') as file1:
+with open('channel.csv', 'r') as channel_file, open('file1.csv', 'r') as file1, open('file2.csv', 'r') as file2, open('file3.csv', 'r') as file3, open('file4.csv', 'r') as file4, open('file5.csv', 'r') as file5, open('file6.csv', 'r') as file6:
     channel_reader = csv.reader(channel_file)
     file1_reader = csv.reader(file1)
     
@@ -19,8 +19,10 @@ with open('channel.csv', 'r') as channel_file, open('file1.csv', 'r') as file1:
     channel=list(channel_reader)
     file1=list(file1_reader)
 
-    for row in file1[:100]:
-        print(row)
+    print(len(file1))
+
+    # for row in file1[:100]:
+    #     print(row)
 
 
 

3edc748c70dcaa6e9852096c58fe4a04e07d3ab8
Andrew Ke <keyanzhe@gmail.com>
2024-05-13 23:39:26 +0100

    add audio of clapping

---
+++ analyse_audio.py
@@ -0,0 +1,30 @@
+
+
+"""
+source '/Users/andrewke/Library/CloudStorage/OneDrive-UniversityofCambridge/2A Audio modem/2A Project - Audio modem Repo/audio_modem/bin/activate'
+python3 '/Users/andrewke/Library/CloudStorage/OneDrive-UniversityofCambridge/2A Audio modem/2A Project - Audio modem
+ Repo/analyse_audio.py'
+ """
+
+import librosa
+import matplotlib.pyplot as plt
+
+# Path to your .m4a file
+file_path = "/Users/andrewke/Library/CloudStorage/OneDrive-UniversityofCambridge/2A Audio modem/2A Project - Audio modem Repo/recordings/iphone.m4a"
+file_path2 = "/Users/andrewke/Library/CloudStorage/OneDrive-UniversityofCambridge/2A Audio modem/2A Project - Audio modem Repo/recordings/ZOOM0009.WAV"
+
+
+# Load the audio file
+audio_data, sr = librosa.load(file_path)
+audio_data2, sr2 = librosa.load(file_path2)
+
+
+# Plot the waveform
+plt.figure(figsize=(12, 4))
+plt.plot(audio_data, label=file_path.split('/')[-1])
+plt.plot(audio_data2, label=file_path2.split('/')[-1])
+plt.xlabel("Sample")
+plt.ylabel("Amplitude")
+plt.title("Waveforms of {} and {}".format(file_path.split('/')[-1], file_path2.split('/')[-1]))
+plt.legend()  # Show legend with file names
+plt.show()

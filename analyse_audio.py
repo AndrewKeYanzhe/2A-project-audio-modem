9b9bdb077cbed09f239ee6c6d83082ec4eeb83a4
Gimlettt <zy353@cam.ac.uk>
2024-05-16 15:02:23 +0100

    not much

--- analyse_audio.py
+++ analyse_audio.py
@@ -1,10 +1,24 @@
 
 
 """
+This script analyzes and plots the waveforms of two audio files using the librosa library.
+
+Librosa is a Python library for audio and music analysis. It provides various functions and tools for audio processing, such as loading audio files, extracting features, and visualizing audio data.
+
+The script loads two audio files, 'chirp_lib.m4a' and 'linear_chirp.wav', using the librosa.load() function. It then plots the waveforms of the audio files using matplotlib.pyplot.plot().
+
+Example usage:
+python3 analyse_audio.py
+"""
+"""
 source '/Users/andrewke/Library/CloudStorage/OneDrive-UniversityofCambridge/2A Audio modem/2A Project - Audio modem Repo/audio_modem/bin/activate'
 python3 '/Users/andrewke/Library/CloudStorage/OneDrive-UniversityofCambridge/2A Audio modem/2A Project - Audio modem
  Repo/analyse_audio.py'
 """
+
+
+
+
 
 import librosa
 import matplotlib.pyplot as plt
@@ -21,10 +35,11 @@ audio_data2, sr2 = librosa.load(file_path2)
 
 # Plot the waveform
 plt.figure(figsize=(12, 4))
-plt.plot(audio_data, label=file_path.split('/')[-1])
+plt.plot(audio_data2, label=file_path.split('/')[-1])
 # plt.plot(audio_data2, label=file_path2.split('/')[-1])
 plt.xlabel("Sample")
 plt.ylabel("Amplitude")
 plt.title("Waveforms of {} and {}".format(file_path.split('/')[-1], file_path2.split('/')[-1]))
 plt.legend()  # Show legend with file names
 plt.show()
+print(type)

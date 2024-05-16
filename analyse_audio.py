a1fdcc68bdb25e81aca8bf231995069906315607
Andrew Ke <keyanzhe@gmail.com>
2024-05-16 15:38:05 +0100

    add mac recording

--- analyse_audio.py
+++ analyse_audio.py
@@ -24,8 +24,8 @@ import librosa
 import matplotlib.pyplot as plt
 
 # Path to your .m4a file
-file_path = "recordings/chirp_lib.m4a"
-file_path2 = "recordings/ZOOM0009.WAV"
+file_path = "recordings/mac_recording.m4a"
+file_path2 = "recordings/chirp_1k_3k.m4a"
 
 
 # Load the audio file

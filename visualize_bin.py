6d8e4f2092b9f686d83424424399423effc7acf4
Matthew_Zhang_Mac <mz473@cam.ac.uk>
2024-05-20 14:07:47 +0100

    trying to extract only 20Hz to 8k Hz bins in OFDM

--- visualize_bin.py
+++ visualize_bin.py
@@ -9,6 +9,34 @@ def bytes_to_binary_string(byte_data):
     binary_string = ''.join(format(byte, '08b') for byte in byte_data)
     return binary_string
 
+
+def extract_OFDM_bins(binary_string, num_bins=1024, fs=48000, f_low=20, f_high=8000):
+    
+    # segment the binary string into chunks of 1022 bits
+    chunk_size = 1022
+    num_chunks = len(binary_string) // chunk_size
+    print("num_chunks:", num_chunks)
+    chunks = [binary_string[i*chunk_size:(i+1)*chunk_size] for i in range(num_chunks)]
+    
+    for i, chunk in enumerate(chunks):
+        # print("Chunk", i, ":", chunk)
+        start_index = f_low * num_bins // fs
+        end_index = f_high * num_bins // fs
+        chunk = chunk[start_index:end_index]
+        # print(chunk[start_index:end_index])
+        
+        # count the number of 1s in the chunk
+        num_ones = chunk.count('1')
+        num_zeros = chunk.count('0')
+        if num_ones > num_zeros:
+            print(f"Chunk {i} is decoded as 1")
+        else:
+            print(f"Chunk {i} is decoded as 0")
+    return chunks
+    
+    
+
+
 # Example usage
 bin_file_path1 = './files/binary_blocks_test_file.bin'
 bin_file_path2 = './files/received_image.bin'
@@ -17,7 +45,9 @@ byte_data2 = load_bin_file(bin_file_path2)
 binary_string1 = bytes_to_binary_string(byte_data1)
 binary_string2 = bytes_to_binary_string(byte_data2)
 
-# Output the binary string (first 100 bits for demonstration)
-print("Binary String1 (first 500 bits):", binary_string1[0:2000])
-print("Binary String2 (first 500 bits):", binary_string2[0:2000])
+# # Output the binary string (first 100 bits for demonstration)
+# print("Binary String1 (first 500 bits):", binary_string1[0:2000])
+# print("Binary String2 (first 500 bits):", binary_string2[0:2000])
 
+# Extract OFDM bins
+extract_OFDM_bins(binary_string2[0:100000])

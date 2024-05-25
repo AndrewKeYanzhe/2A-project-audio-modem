df6cbde4a5c3c6cbe376fb17f61767dab28f49f4
Matthew_Zhang_Mac <mz473@cam.ac.uk>
2024-05-25 14:30:03 +0100

    Modified utils for cutting frequency bins

--- utils.py
+++ utils.py
@@ -14,10 +14,16 @@ def save_as_wav(signal, file_path, fs):
     logging.info(f"Signal has been saved to {file_path}.")
     
 def cut_freq_bins(f_low, f_high, fs, n_bins):
-    """Returns the indices of the frequency bins corresponding to the given frequency range."""
-    n_low = np.ceil(f_low * n_bins / fs)
-    n_high = np.floor(f_high * n_bins / fs)
-    return int(n_low), int(n_high)
+    """
+    Returns the indices of the frequency bins corresponding to the given frequency range.
+    If f_low and f_high are None, the function returns 0 and n_bins//2.
+    """
+    if f_low is None or f_high is None:
+        return 1, n_bins//2
+    else:
+        n_low = np.ceil(f_low * n_bins / fs)
+        n_high = np.floor(f_high * n_bins / fs)
+        return int(n_low), int(n_high)
 
 if __name__ == "__main__":
     assert cut_freq_bins(0, 24000, 48000, 4096) == (0, 2048)
